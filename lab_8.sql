402d1614486ba73b9fb259539f00cd10c52e465a
Chris Lacarte <chrislacarte@icloud.com>
2022-06-24 17:24:59 +0100

    8 and 9 not completed

--- lab_8.sql
+++ lab_8.sql
@@ -25,27 +25,31 @@ GROUP BY C.store_id
 ORDER BY sum(P.amount);
 
 
--- 3.Which film categories are longest?
-SELECT C.name, F.film_id, FI.length 
+-- 3.Which film categories are longest , I did it as average lenght of the category 
+SELECT DISTINCT(C.name) as Category , AVG(FI.length) as "Average Length"
 FROM category C
 JOIN film_category F
 ON C.category_id = F.category_id
 JOIN film FI
 ON FI.film_id = F.film_id
-GROUP BY C.name, F.film_id
+GROUP BY C.name
 ORDER BY FI.length desc;
 
--- 4.Display the most frequently rented movies in descending order.
-
-
-
-
-
+-- 4.Display the most frequently rented movies in descending order. I show the 10 most rented
+SELECT F.title,COUNT(I.film_id) as number_of_rentals
+FROM rental R
+JOIN inventory I 
+ON R.inventory_id = I.inventory_id
+JOIN film F
+ON I.film_id = F.film_id
+GROUP BY F.title
+ORDER BY number_of_rentals DESC
+LIMIT 10;
 
 
 -- 5.List the top five genres in gross revenue in descending order.
 
-SELECT C.name,SFC.total_sales
+SELECT C.name AS Category,SFC.total_sales
 FROM film F
 JOIN film_category FC
 ON F.film_id = FC.film_id
@@ -58,20 +62,24 @@ ORDER BY SFC.total_sales desc
 LIMIT 5;
 
 -- 6.Is "Academy Dinosaur" available for rent from Store 1?
-SELECT I.inventory_id, F.title, I.store_id
+SELECT COUNT(I.inventory_id) AS "Amount available", F.title, I.store_id
 FROM inventory I 
 JOIN film F 
 ON I.film_id = F.film_id
-WHERE F.title = "Academy Dinosaur" AND I.store_id = 1;
+WHERE F.title = "Academy Dinosaur" AND I.store_id = 1
+GROUP BY F.title;
 
+SELECT * FROM film;
 
 -- 7.Get all pairs of actors that worked together.
 
-SELECT A1.last_name, A2.last_name
-FROM actor A1
-JOIN actor A2
-ON (A1.last_name <> A2.last_name AND (A1.film_id = A2.film_id)
-ORDER BY A1.film_id;
+SELECT A1.actor_id as "1stActor", A2.actor_id as '2ndActor', F.title as Movie
+FROM film_actor A1
+JOIN film_actor A2
+ON (A1.actor_id <> A2.actor_id) AND (A1.film_id = A2.film_id) 
+JOIN film F 
+ON F.film_id = A2.film_id
+ORDER BY A1.actor_id;
 
 
 
